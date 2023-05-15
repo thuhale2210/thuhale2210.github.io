4f127e22b6906371a527341ea9dc815724373d5e
thuhale2210 <thuhale2210@gmail.com>
2023-05-15 12:56:06 -0400

    Deploy website

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PORTFOLIO",
+  base: "/thuhale2210.github.io",
   plugins: [react()],
 })
